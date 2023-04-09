210a58e407baeb18e64af4f420fd73075a1dd2a6
CLblDL <denil.lobanov1994@gmail.com>
2023-04-09 23:33:24 +0300

    Добавьте файлы проекта.

---
+++ LessonsIJunior/Lesson2/Lesson2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessonsIJunior.Lesson2
+{
+    class Lesson2
+    {
+        private string name;
+        private int age;
+        private bool started;
+        private float temperature;
+        private double distance;
+        private decimal price;
+        private byte numberOfSeats;
+        private short speedLimit;
+        private long population;
+        private char grade;
+    }
+}
